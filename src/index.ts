4cfc018299127def1dd5ed158b07fde45d75b5e7
htrobles <hjt.robles@gmail.com>
2024-02-06 00:03:25 -0500

    initialize typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port = 3000; // You can choose any port
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World from Express and TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
