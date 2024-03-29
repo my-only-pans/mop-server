a4a8fea67557d85028e99b194165e5d42bcaae9b
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-29 15:27:05 -0400

    populate owner

--- src/api/controllers/recipe/getRecipe.ts
+++ src/api/controllers/recipe/getRecipe.ts
@@ -4,7 +4,9 @@ import { MRecipe } from '../../../models/Recipe';
 export default async function getRecipe(req: Request, res: Response) {
   const { _id } = req.params;
 
-  const recipe = await MRecipe.findById(_id).lean();
+  const recipe = await MRecipe.findById(_id)
+    .populate('owner', { username: 1, _id: 1 })
+    .lean();
 
   res.json(recipe);
 }
