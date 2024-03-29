a2abeb039dd4dac4abea9a29ab419bf308febed8
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-29 19:22:23 -0400

    update getRecipe and getRecipeDraft

--- src/api/controllers/recipe/getRecipe.ts
+++ src/api/controllers/recipe/getRecipe.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 import { MRecipe } from '../../../models/Recipe';
 
 export default async function getRecipe(req: Request, res: Response) {
-  const { _id } = req.params;
+  try {
+    const { _id } = req.params;
 
-  const recipe = await MRecipe.findById(_id)
-    .populate('owner', { username: 1, _id: 1 })
-    .lean();
+    const recipe = await MRecipe.findById(_id)
+      .populate('owner', { username: 1, _id: 1 })
+      .lean();
 
-  res.json(recipe);
+    res.json(recipe);
+  } catch (error) {
+    res.status(400).send({ error: 'Recipe not found' });
+  }
 }
