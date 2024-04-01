c55e5f4b8319cde01d1d421ec8e792edd584ac62
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-31 23:46:21 -0400

    .gitignore

--- src/api/controllers/recipe/createRecipeDraft.ts
+++ src/api/controllers/recipe/createRecipeDraft.ts
@@ -16,28 +16,29 @@ export default async function createRecipeDraft(
   { body, userId }: AuthenticatedRequest<any, RecipeDraftInput>, // Annotate body with RecipeDraftInput
   res: Response
 ) {
-  const {
-    title,
-    description,
-    prepTime,
-    cookTime,
-    serving,
-    categories,
-    ...rest
-  } = body;
-
-  if (Object.keys(rest).length)
-    return res.status(400).json({ message: 'Invalid data received' });
-  if (!title) return res.status(400).json({ message: 'Title is required' });
-  if (!description)
-    return res.status(400).json({ message: 'Description is required' });
-  if (!prepTime)
-    return res.status(400).json({ message: 'Prep Time is required' });
-  if (!cookTime)
-    return res.status(400).json({ message: 'Cook Time is required' });
-  if (!serving) return res.status(400).json({ message: 'Serving is required' });
-
   try {
+    const {
+      title,
+      description,
+      prepTime,
+      cookTime,
+      serving,
+      categories,
+      ...rest
+    } = body;
+
+    if (Object.keys(rest).length)
+      return res.status(400).json({ message: 'Invalid data received' });
+    if (!title) return res.status(400).json({ message: 'Title is required' });
+    if (!description)
+      return res.status(400).json({ message: 'Description is required' });
+    if (!prepTime)
+      return res.status(400).json({ message: 'Prep Time is required' });
+    if (!cookTime)
+      return res.status(400).json({ message: 'Cook Time is required' });
+    if (!serving)
+      return res.status(400).json({ message: 'Serving is required' });
+
     const draft = (
       await new MRecipeDraft({
         owner: userId,
